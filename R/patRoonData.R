816c378deed3af0d05e7c124b3d7d80465b438b5
Rick Helmus <rhelmus@gmail.com>
2018-03-08 11:45:34 +0100

    initial commit

---
+++ R/patRoonData.R
@@ -0,0 +1,12 @@
+#' Test data for the patRoon package
+#'
+#' A small set of LC-HRMS data to test and demonstrate the functionality of
+#' patRoon.
+#'
+#' This package contains data files from (a) a standard mixture containing
+#' various polar contaminants and (b) a blank solvent. Both were analysed in
+#' triplicate, using an Shimadzu Nexera UHPLC coupled to an high resolution
+#' quadrupole time of flight instrument (Bruker maXis 4G Q-TOF).
+#'
+#' @name patRoonData
+NULL
