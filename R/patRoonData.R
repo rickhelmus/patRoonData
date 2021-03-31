5fa8d9ae9d9d0dc16a4065986999b9aa8f32406b
rickhelmus <r.helmus@uva.nl>
2021-03-31 09:44:34 +0200

    add new pos/neg data

--- R/patRoonData.R
+++ R/patRoonData.R
@@ -1,29 +1,29 @@
 #' Test data for the patRoon package
 #'
-#' A small set of LC-HRMS data to test and demonstrate the functionality of
-#' patRoon.
+#' A small set of LC-HRMS data to test and demonstrate the functionality of patRoon.
 #'
-#' This package contains data files from (a) a standard mixture containing
-#' various polar contaminants and (b) a blank solvent. Both were analysed in
-#' triplicate, using an Shimadzu Nexera UHPLC coupled to an high resolution
-#' quadrupole time of flight instrument (Bruker maXis 4G Q-TOF) using positive
-#' electrospray ionization.
+#' This package contains data files from (1) a standard mixture containing various polar contaminants and (2) a blank
+#' solvent. Both were analysed in triplicate, using an Shimadzu Nexera UHPLC coupled to an high resolution quadrupole
+#' time of flight instrument (Bruker maXis 4G Q-TOF) using positive and negative electrospray ionization.
 #'
-#' To reduce file sizes, the data has been filtered to only contain the first
-#' ten minutes of the chromatographic run with \emph{m/z} 75-300. This filtering
-#' step was performed with the \command{msconvert} command of
-#' \href{http://proteowizard.sourceforge.net/index.shtml}{ProteoWizard}. The
-#' complete data processing script can be found in data-raw/process.R
+#' To reduce file sizes, the data has been filtered to only contain the first ten minutes of the chromatographic run
+#' with \emph{m/z} 75-500. This filtering step was performed with the \command{msconvert} command of
+#' \href{http://proteowizard.sourceforge.net/index.shtml}{ProteoWizard}. The complete data processing script can be
+#' found in data-raw/process.R
 #'
 #' @name patRoonData
 NULL
 
-#' @details \code{exampleDataPath} is a helper function that returns the file
-#'   path of the example data.
-#'
+#' @details \code{exampleDataPath} is a helper function that returns the file path of the example data.
+#' @param polarity Either \code{"positive"} or \code{"negative"}.
 #' @rdname patRoonData
 #' @export
-exampleDataPath <- function() system.file("extdata", package = "patRoonData")
+exampleDataPath <- function(polarity = "positive")
+{
+    if (length(polarity) != 1 || !polarity %in% c("positive", "negative"))
+        stop("polarity should be positive or negative", call. = FALSE)
+    system.file(file.path("extdata", if (polarity == "positive") "pos" else "neg"), package = "patRoonData")
+}
 
 
 #' Example target analyte list
--- data-raw/process.R
+++ data-raw/process.R
@@ -1,33 +1,26 @@
 library(patRoon)
 
-dataDir <- "~/werk/Vittorio/demo"
-anaInfo <- generateAnalysisInfo(dataDir, fileTypes = "Bruker")
-
-# data recalibration & export
-setDAMethod(anaInfo, file.path(dataDir, "20180306-DA-pos.m"))
-recalibrarateDAFiles(anaInfo)
-exportDAFiles(anaInfo)
-
-# trim files: only keep retention/mz ranges specified below
-# rtRange <- "0:600"; mzRange <- "75:300"
-rtRange <- c(0L, 600L); mzRange <- c(75L, 300)
-for (anai in seq_len(nrow(anaInfo)))
+for (pol in c("pos", "neg"))
 {
-    mzML <- file.path(anaInfo$path[anai], paste0(anaInfo$analysis[anai], ".mzML"))
-    mzMLOut <- file.path(anaInfo$path[anai], paste0(anaInfo$analysis[anai], "-compr.mzML"))
-    # BUG: OpenMS seems to botch MS/MS precursor mz values (at least when loading processed files with mzR)
-    # system2("FileFilter", c("-in", mzML, "-rt", rtRange, "-mz", mzRange, "-out", mzMLOut))
-    # pwiz seems to work though...
-    system2("msconvert", sapply(c(mzML, "--filter", sprintf("scanTime [%d,%d]", rtRange[1], rtRange[2]),
-                                  "--filter", sprintf("mzWindow [%d,%d]", mzRange[1], mzRange[2]),
-                                  "--outfile", mzMLOut), shQuote))
+    dataDir <- paste0("C:/Users/rick/OneDrive - UvA/patRoon/patRoonData20/", pol)
+    anaInfo <- generateAnalysisInfo(dataDir, formats = "bruker")
+
+    # data recalibration
+    setDAMethod(anaInfo, file.path(dataDir, paste0("20210331-DA-", pol, ".m")))
+    recalibrarateDAFiles(anaInfo)
+
+    # export & trim files: only keep retention/mz ranges specified below
+    rtRange <- c(0L, 600L); mzRange <- c(75L, 450L)
+    convertMSFiles(anaInfo = anaInfo, from = "bruker", to = "mzML",
+                   filters = c(sprintf("scanTime [%d,%d]", rtRange[1], rtRange[2]),
+                               sprintf("mzWindow [%d,%d]", mzRange[1], mzRange[2])),
+                   extraOpts = "-n", overWrite = TRUE)
+    
+    # simplify sample names
+    simplAnaNames <- anaInfo$analysis
+    simplAnaNames[grepl("blank", simplAnaNames, fixed = TRUE)] <- paste0("solvent-", pol, "-", 1:3)
+    simplAnaNames[grepl("std", simplAnaNames, fixed = TRUE)] <- paste0("standard-", pol, "-", 1:3)
+
+    file.rename(file.path(anaInfo$path, paste0(anaInfo$analysis, ".mzML")),
+                file.path(anaInfo$path, paste0(simplAnaNames, ".mzML")))
 }
-
-# simplify sample names
-simplAnaNames <- sub("20180305-ECROS_STD_10_", "standard-", anaInfo$analysis)
-simplAnaNames <- sub("20180305-solvent_A_([0-9])", "solvent-\\1", simplAnaNames)
-simplAnaNames <- sub("-[0-9]+-pos[A-Za-z0-9_\\-]+", "", simplAnaNames)
-simplAnaNames <- paste0(simplAnaNames, "-", rep(seq_len(3), 2))
-
-file.rename(file.path(anaInfo$path, paste0(anaInfo$analysis, "-compr.mzML")),
-            file.path(anaInfo$path, paste0(simplAnaNames, ".mzML")))
