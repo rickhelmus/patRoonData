79066acc27f7fb202e16032b9eb5118c42073d43
rickhelmus <r.helmus@uva.nl>
2021-03-31 14:57:41 +0200

    exampleAnalysisInfo()

--- R/patRoonData.R
+++ R/patRoonData.R
@@ -7,24 +7,44 @@
 #' time of flight instrument (Bruker maXis 4G Q-TOF) using positive and negative electrospray ionization.
 #'
 #' To reduce file sizes, the data has been filtered to only contain the first ten minutes of the chromatographic run
-#' with \emph{m/z} 75-500. This filtering step was performed with the \command{msconvert} command of
-#' \href{http://proteowizard.sourceforge.net/index.shtml}{ProteoWizard}. The complete data processing script can be
+#' with \emph{m/z} 75-500. The complete data processing script can be
 #' found in data-raw/process.R
+#' 
+#' @param polarity Either \code{"positive"} or \code{"negative"}.
 #'
 #' @name patRoonData
 NULL
 
+assertPolarity <- function(polarity)
+{
+    if (length(polarity) != 1 || !polarity %in% c("positive", "negative"))
+        stop("polarity should be positive or negative", call. = FALSE)
+}
+
 #' @details \code{exampleDataPath} is a helper function that returns the file path of the example data.
-#' @param polarity Either \code{"positive"} or \code{"negative"}.
 #' @rdname patRoonData
 #' @export
 exampleDataPath <- function(polarity = "positive")
 {
-    if (length(polarity) != 1 || !polarity %in% c("positive", "negative"))
-        stop("polarity should be positive or negative", call. = FALSE)
+    assertPolarity(polarity)
     system.file(file.path("extdata", if (polarity == "positive") "pos" else "neg"), package = "patRoonData")
 }
 
+#' @details \code{exampleAnalysisInfo} is a helper function that generate analysis information that can be directly used
+#'   by \pkg{patRoon}.
+#' @rdname patRoonData
+#' @export
+exampleAnalysisInfo <- function(polarity = "positive")
+{
+    assertPolarity(polarity)
+    if (!requireNamespace("patRoon", quietly = TRUE))
+        stop("Please make sure that patRoon is installed!", call. = FALSE)
+    suffix <- if (polarity == "positive") "pos" else "neg"
+    patRoon::generateAnalysisInfo(exampleDataPath(polarity),
+                                  groups = c(rep(paste0("solvent-", suffix), 3),
+                                             rep(paste0("standard-", suffix), 3)),
+                                  blanks = paste0("solvent-", suffix))
+}
 
 #' Example suspect lists
 #'
